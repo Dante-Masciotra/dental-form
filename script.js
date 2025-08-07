bd697f0e0c4181fd04cc35a8bc0d7cc07a4a1ebe
Dante Masciotra <jazzy.d1302@outlook.com>
2025-08-07 15:01:37 -0400

    improved readabity and site name

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ function saveForm() {
   const comments = document.getElementById("comments").value.trim();
 
   const content = 
-`Name: ${name}
-Email: ${email}
-Age: ${age}
-Favorite Color: ${color}
-Comments:
-${comments}
-`;
+    `Name: ${name}
+    Email: ${email}
+    Age: ${age}
+    Favorite Color: ${color}
+    Comments:
+    ${comments}
+    `;
 
-  const blob = new Blob([content], { type: "text/plain" });
-  const link = document.createElement("a");
+    const blob = new Blob([content], { type: "text/plain" });
+    const link = document.createElement("a");
 
-  link.download = "form-output.txt";
-  link.href = URL.createObjectURL(blob);
-  link.click();
-  URL.revokeObjectURL(link.href);
+    link.download = "form-output.txt";
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
 }
